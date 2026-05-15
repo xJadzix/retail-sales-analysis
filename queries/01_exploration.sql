7debcf28940d225d78ea1c61099de33a00854bf1
Jagoda <42112821+xJadzix@users.noreply.github.com>
2026-05-15 11:57:38 +0200

    Update 01_exploration.sql
    Add exploration queries

--- queries/01_exploration.sql
+++ queries/01_exploration.sql
@@ -1,1 +1,25 @@
+-- How many rows in the table?
+SELECT COUNT(*) FROM online_retail;
 
+-- How do the rows look like?
+SELECT * FROM online_retail LIMIT 10;
+
+-- How many unique countries?
+SELECT COUNT(DISTINCT "Country") FROM online_retail;
+
+-- What countries do we have?
+SELECT DISTINCT "Country" FROM online_retail ORDER BY "Country";
+
+-- How many unique clients?
+SELECT COUNT(DISTINCT "CustomerID") FROM online_retail;
+
+-- Are there any null values in CustomerID?
+SELECT COUNT(*) FROM online_retail WHERE "CustomerID" IS NULL;
+
+-- Transactions with price = 0 - suspicious cases
+SELECT * FROM online_retail WHERE "UnitPrice" = 0;
+
+-- How many returns (InvoiceNo starting with C)?
+SELECT COUNT(*) AS num_returns
+FROM online_retail
+WHERE "InvoiceNo" LIKE 'C%';
